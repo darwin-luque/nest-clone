77f76d5a3e514c449485cf5fd6e278e0857b6acf
Darwin Luque <darwin.luque.98@gmail.com>
2022-02-11 16:36:15 -0600

    chore: instantiate app router

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-import './classes/example';
+import express from 'express';
+import { AppRouter } from './app-router';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(AppRouter.getInstance());
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
